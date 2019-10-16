192d936894fea22fe25810e95c32b3ac47628840
narirock <nari9rock@hotmail.com>
2019-10-16 17:59:56 -0300

    first commit

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        echo "\n";
+        echo "iniciou uma tarefa";
+        sleep(30);
+        var_dump($this->user);
+        echo "finalizou";
+    }
+}
